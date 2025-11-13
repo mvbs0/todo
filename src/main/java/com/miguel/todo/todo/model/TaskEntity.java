51e9e38c8f9a6f6ed98f7754eba4411d90783334
mvbs0 <m.vbs1281@gmail.com>
2025-11-13 09:45:33 -0300

    create TaskEntity

---
+++ src/main/java/com/miguel/todo/todo/model/TaskEntity.java
@@ -0,0 +1,30 @@
+package com.miguel.todo.todo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity (name = "task")
+public class TaskEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column (nullable = false)
+    private Date dateOfDelivery;
+    @Column (nullable = false)
+    private Date dateOfStart;
+    @Column (nullable = false)
+    private String description;
+    @Column (nullable = false)
+    private String title;
+
+}
